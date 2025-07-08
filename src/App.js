fba79c0dfb99ed1f2b768b07dc981b02ba002ec3
erik.minasian <eminasian@webmd.net>
2025-07-08 16:54:17 +0400

    fix: format

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 const Pizaa = (props) => {
-  return React.createElement(
-    "div", 
-    {}, 
-    [
-      React.createElement("h1", {}, props.name),
-      React.createElement("p", {}, props.description)
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div", 
-    {},
-    [
-      React.createElement("h1", {}, "Padre Gino's"),
-      React.createElement(Pizaa, {name: 'name 1', description: "description 1"}),
-      React.createElement(Pizaa, {name: 'name 2', description: "description 2"}),
-      React.createElement(Pizaa, {name: 'name 3', description: "description 3"}),
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizaa, {
+      name: "name 1",
+      description: "description 1",
+    }),
+    React.createElement(Pizaa, {
+      name: "name 2",
+      description: "description 2",
+    }),
+    React.createElement(Pizaa, {
+      name: "name 3",
+      description: "description 3",
+    }),
+  ]);
+};
 
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
