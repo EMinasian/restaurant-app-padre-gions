7798f44516ba05c004e17760f41cdf777392c644
erik.minasian <eminasian@webmd.net>
2025-07-16 10:27:12 +0400

    feat: jsx instead of vanilla react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-const Pizaa = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
+import React from "react";
+import { createRoot } from 'react-dom/client'
+import Pizza from "./components/Pizza";
+
+// const Pizza = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("p", {}, props.description),
+//   ]);
+// };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
-    React.createElement(Pizaa, {
+    React.createElement(Pizza, {
       name: "name 1",
       description: "description 1",
     }),
-    React.createElement(Pizaa, {
+    React.createElement(Pizza, {
       name: "name 2",
       description: "description 2",
     }),
-    React.createElement(Pizaa, {
+    React.createElement(Pizza, {
       name: "name 3",
       description: "description 3",
     }),
@@ -24,5 +28,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
